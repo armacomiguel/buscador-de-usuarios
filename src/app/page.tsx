dc1a4bb5b9fab0d24e9f827466db86a90d649d96
armacomiguel <armacomiguel@gmail.com>
2024-06-25 11:14:53 -0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const Home = () => {
           <SearchIcon className='fill-sky-500'
           />
         </span>
-        <input type='text' placeholder='Search Github usuario...' className='flex-1 h-14 p-2 rounded-lg 
+        <input type='text' placeholder='Buscar Github usuario...' className='flex-1 h-14 p-2 rounded-lg 
         bg-transparent text-white placeholder:text-white focus:outline-none focus:ring-2 focus:ring-sky-500'/>
         <button className='bg-sky-500 rounded-lg py-4 px-4 text-white font-bold'>Buscar</button>
       </form>
